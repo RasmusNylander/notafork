0b4e05492a5b405bda69c571727de2ab320e782d
Rasmus Nylander <rany@trackman.com>
2024-03-04 12:52:30 +0100

    data: Save performer id with pose

--- tools/convert_h36m.py
+++ tools/convert_h36m.py
@@ -3,23 +3,34 @@ import sys
 import pickle
 from pathlib import Path
 
+from numpy import ndarray
+
 sys.path.insert(0, os.getcwd())
 from lib.data.datareader_h36m import DataReaderH36M
 from tqdm import tqdm
 
 
-def save_clips(subset_name, root_path, data, labels):
+def save_clips(
+        subset_name: str,
+        root_path: Path,
+        pose_2d: list[list[ndarray]],
+        target: list[list[ndarray]],
+        performers: list[int],
+):
     save_path = root_path / subset_name
     if not save_path.exists():
         save_path.mkdir(parents=True)
     i = 0
-    for performer, performer_label in tqdm(zip(data, labels), desc=f"Saving {subset_name} data", unit="performer"):
-        for activity, activity_label in tqdm(zip(performer, performer_label), leave=False, unit="activity"):
-            activity, activity_label = activity.swapaxes(0, 1), activity_label.swapaxes(0, 1)
-            for batch, label in zip(activity, activity_label):
+    for performer_2d, performer_target, performer in tqdm(list(zip(pose_2d, target, performers)), desc=f"Saving {subset_name} data", unit="performer"):
+        for activity_2d, activity_target in zip(performer_2d, performer_target):
+            activity_2d, activity_target = activity_2d.swapaxes(0, 1), activity_target.swapaxes(0, 1)
+            for input, target in zip(activity_2d, activity_target):
                 data_dict = {
-                    "data_input": batch,
-                    "data_label": label
+                    "data_input": input,
+                    "data_label": target,
+                    "meta": {
+                        "performer": performer,
+                    }
                 }
                 with open(save_path / f"{i:08d}.pkl", "wb") as file:
                     pickle.dump(data_dict, file)
@@ -36,6 +47,6 @@ root_path = data_root / "MB3D_f243s81" / "H36M-SH"
 if not root_path.exists():
     root_path.mkdir(parents=True)
 
-save_clips("train", root_path, train_data, train_labels)
-save_clips("test", root_path, test_data, test_labels)
+save_clips("train", root_path, train_data, train_labels, [1, 5, 6, 7, 8])
+save_clips("test", root_path, test_data, test_labels, [9, 11])
 
